5b3b5d1626350bb079d45038253cfbea135e4121
Feruza Saidkhodjaeva <jofe@Feruzas-Air.hsd1.pa.comcast.net>
2022-12-27 18:36:07 -0500

    adding the securty group

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "project_vpc" {
-    cidr_block = "10.0.0.0/16"
-    
+  cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "project-1VPC"
   }
@@ -40,8 +40,8 @@ resource "aws_route_table" "project_rt" {
     gateway_id = aws_internet_gateway.gw.id
   }
 
- 
-    
+
+
   tags = {
     Name = "project-1rttable"
   }
@@ -55,4 +55,79 @@ resource "aws_route_table_association" "a" {
 resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.projectsubnet2.id
   route_table_id = aws_route_table.project_rt.id
+}
+
+## security groups for terraform project
+resource "aws_security_group" "terraform_sg" {
+  name        = "terraform_sg"
+  description = "SG for default page"
+  vpc_id      = aws_vpc.project_vpc.id
+  ingress {
+    description = "ssh to my ip"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "http to my ip"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "terraform_sg"
+  }
+}
+
+
+## load balancer security_group
+
+resource "aws_security_group" "terraform_alb_sg" {
+  name        = "terraform_alb_sg"
+  description = "SG for ALB"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress {
+    description = "ssh from my ip"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "terraform_alb_sg"
+  }
 }
